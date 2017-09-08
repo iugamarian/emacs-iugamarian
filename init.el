49e7947a82c32b241e73d65e985c3a4811a451a9
Iuga Marian Silviu <iuga.marian@gmail.com>
2017-09-08 21:32:14 +0300

    Font
    Font

--- init.el
+++ init.el
@@ -1,2 +1,4 @@
-(set-foreground-color "green")
+(set-foreground-color "gray")
 (set-background-color "black")
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono 22" ))
+(set-face-attribute 'default t :font "DejaVu Sans Mono 22" )
